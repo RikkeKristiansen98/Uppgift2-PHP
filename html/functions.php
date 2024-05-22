e321838a045bced8b3a5dd33c75391f48c30cbeb
Rikke Kristiansen <rikke.kristiansen@hotmail.com>
2024-05-22 16:27:58 +0200

    Registrera ny användare

--- html/functions.php
+++ html/functions.php
@@ -1,13 +1,28 @@
-<?php function get_name($name = null) {
-            if($name) {
-                return $name;
-            }
+<?php 
+function connect_database() {
+    $host = "db";
+    $username = "root";
+    $password = "notSecureChangeMe";
+    $database = "uppgift2";
 
-            return "stranger";
-        }
+    $mysqli = new mysqli($host, $username, $password, $database);
 
-        function get_window_title($title) {
-            return $title.' - My awesome site';
-        }
+    if ($mysqli->connect_error) {
+        die('Connect Error (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error);
+    }
 
-        ?>
+    return $mysqli;
+}
+
+function get_name($name = null) {
+    if ($name) {
+        return $name;
+    }
+
+    return "stranger";
+}
+
+function get_window_title($title) {
+    return $title . ' - My awesome site';
+}
+?>
