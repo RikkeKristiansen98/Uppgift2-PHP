839a72e306ece690a7f70ef97ffbc9af1d7e46a1
Rikke Kristiansen <rikke.kristiansen@hotmail.com>
2024-05-23 11:09:29 +0200

    Logga in

--- html/signUp.php
+++ html/signUp.php
@@ -1,11 +1,11 @@
 <h1>Skapa ett konto</h1>
 
 <form action="subscribe.php" method="post">
-    <label for="firstname">Firstname: <input type="text" name="firstname" required></label><br>
-    <label for="lastname">Lastname: <input type="text" name="lastname" required></label><br>
+    <label for="firstname">Namn: <input type="text" name="firstname" required></label><br>
+    <label for="lastname">Efternamn: <input type="text" name="lastname" required></label><br>
     <label for="email">Email: <input type="email" name="email" required></label><br>
-    <label for="password">Password: <input type="password" name="password" required></label><br>
-    <label for="role">Role: 
+    <label for="password">Lösenord: <input type="password" name="password" required></label><br>
+    <label for="role">Jag är: 
         <select name="role" required>
             <option value="customer">Kund</option>
             <option value="subscriber">Prenumerant</option>
